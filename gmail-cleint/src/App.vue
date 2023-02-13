aec57d52c24c7cf256a2f8be67aca979c500064d
Dagi <dagi.zewdu.dz@gmail.com>
2023-02-13 09:08:27 +0300

    Added vue3 login

--- gmail-cleint/src/App.vue
+++ gmail-cleint/src/App.vue
@@ -1,17 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="container">
+    <button class="btn" @click="login">open G-mail</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { googleSdkLoaded } from 'vue3-google-login';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  methods: {
+    login() {
+      googleSdkLoaded((google) => {
+        google.accounts.oauth2
+          .initCodeClient({
+            client_id: '939872883657-bbbp12lu78ld0ukbfnoc1njbn3rpv2ku.apps.googleusercontent.com',
+            scope: 'https://www.googleapis.com/auth/gmail.readonly https://mail.google.com/',
+
+            callback: (response) => {
+              console.log(response);
+            },
+          })
+          .requestCode();
+      });
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +36,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container {
+  min-height: 90vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.btn {
+  padding: 10px;
+  color: #f47141;
 }
 </style>
--- gmail-cleint/src/main.js
+++ gmail-cleint/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vue3GoogleLogin from 'vue3-google-login'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(vue3GoogleLogin, {
+    clientId: '939872883657-bbbp12lu78ld0ukbfnoc1njbn3rpv2ku.apps.googleusercontent.com',
+    scope: "https://www.googleapis.com/auth/gmail.readonly"
+})
+app.mount('#app')
